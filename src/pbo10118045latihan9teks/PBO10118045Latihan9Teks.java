0324988a1c2fffcdae5243a0851b4c6f00f222ed
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 20:55:07 +0700

    first commit

---
+++ src/pbo10118045latihan9teks/PBO10118045Latihan9Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan9teks;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : teks
+ */
+public class PBO10118045Latihan9Teks {
+
+    
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" +newline+ "char: " +tab+ "berisi " +c);
+        
+    }
+    
+}
